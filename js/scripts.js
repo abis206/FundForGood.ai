9a645de5431982079312f54897a3f0ecfae2af4a
Abigayle Peterson <abigaylerose03@gmail.com>
2020-06-01 00:47:28 -0700

    basic tab toggle login/register box

--- js/scripts.js
+++ js/scripts.js
@@ -211,8 +211,19 @@
 		$(this).blur();
 	});
 
-    $('.tab a').on('click', function(e) {
-
+    /* Switches from sign up to login on the login-register page */
+    $('#login-form-link').click(function(e) {
+        $("#login-form").delay(100).fadeIn(100);
+        $("#register-form").fadeOut(100);
+        $('#register-form-link').removeClass('active');
+        $(this).addClass('active');
+        e.preventDefault();
     });
-
+    $('#register-form-link').click(function(e) {
+        $("#register-form").delay(100).fadeIn(100);
+        $("#login-form").fadeOut(100);
+        $('#login-form-link').removeClass('active');
+        $(this).addClass('active');
+        e.preventDefault();
+    });
 })(jQuery);
